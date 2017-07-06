e42cba302447c64e3debf5d5bf5b1a6a9a10dade
root <root@61949b3d2f95>
2017-07-06 03:31:18 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_WmqiPDHZfl",
+  "homePage" : "Main",
+  "name" : "Auto_WmqiPDHZfl",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
